fbd9a8aef632e6f4ac7aaec8479e8b8c778a78d7
chacha86 <taejincha0619@gmail.com>
2025-10-31 12:15:24 +0900

    rq2

--- src/main/kotlin/com/back/App.kt
+++ src/main/kotlin/com/back/App.kt
@@ -40,7 +40,13 @@ class App {
                 }
 
                 "삭제" -> {
-                    val id = rq.getParamValue("id")
+                    val id = rq.getParamValueAsInt("id", 0)
+
+                    if(id == 0) {
+                        println("id를 정확히 입력해주세요.")
+                        continue
+                    }
+
                     println("${id}")
                 }
             }
