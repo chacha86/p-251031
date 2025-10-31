238a03248ef78f86f09db351a35f234956e5d04d
chacha86 <taejincha0619@gmail.com>
2025-10-31 11:06:06 +0900

    1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,4 +3,16 @@ package com.back
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+
+    println("== 명언 앱 ==")
+
+    while (true) {
+        print("명언) ")
+        val input = readln().trim()
+
+        if(input == "종료") {
+            break
+        }
+    }
+
 }
