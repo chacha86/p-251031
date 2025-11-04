0f07dae224817968c3b2b5b79741a6414fba3d82
chacha86 <taejincha0619@gmail.com>
2025-11-04 10:36:28 +0900

    refactoring & singletonScope

--- src/main/kotlin/com/back/App.kt
+++ src/main/kotlin/com/back/App.kt
@@ -2,11 +2,12 @@ package com.back
 
 import com.back.domain.system.controller.SystemController
 import com.back.domain.wiseSaying.controller.WiseSayingController
+import com.back.global.bean.SingletonScope
 import com.back.global.rq.Rq
 
 class App(
-    val wiseSayingController: WiseSayingController = WiseSayingController(),
-    val systemController: SystemController = SystemController()
+    val wiseSayingController: WiseSayingController = SingletonScope.wiseSayingController,
+    val systemController: SystemController = SingletonScope.systemController
 ) {
 
     fun run() {
